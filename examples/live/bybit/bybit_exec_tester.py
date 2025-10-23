206536c1c4b666da5857a99efa481f6166afc9af
Chris Sellers <chris@cjdsellers.io>
2025-10-24 08:53:54 +1000

    Continue Bybit adapter port to Rust

--- examples/live/bybit/bybit_exec_tester.py
+++ examples/live/bybit/bybit_exec_tester.py
@@ -190,7 +190,9 @@ config_tester = ExecTesterConfig(
     # test_reject_post_only=True,
     reduce_only_on_stop=False,  # Not supported for Bybit SPOT
     # cancel_orders_on_stop=False,
-    close_positions_on_stop=False,
+    # close_positions_on_stop=False,
+    # use_batch_cancel_on_stop=True,
+    # use_individual_cancels_on_stop=True,
     log_data=False,
     log_rejected_due_post_only_as_warning=False,
 )
